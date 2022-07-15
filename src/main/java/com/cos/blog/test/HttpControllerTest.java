fc0d61243570eeb116dba0195856242237c885c3
eric9883 <eric9883@naver.com>
2022-07-15 15:21:37 +0900

    get, post, put, delete

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -5,21 +5,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    //http://localhost:8080/http/get
     @GetMapping("/http/get")
     public String getTest(){
         return "get request";
     }
 
+    //http://localhost:8080/http/post
     @PostMapping("/http/post")
     public String postTest(){
         return "post request";
     }
 
+    //http://localhost:8080/http/put
     @PutMapping("/http/put")
     public String putTest(){
         return "put request";
     }
 
+    //http://localhost:8080/http/delete
     @DeleteMapping("/http/delete")
     public String deleteTest(){
         return "delete request";
