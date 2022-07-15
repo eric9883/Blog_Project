4277af9cfec3fee95dd6694ce49b2964ed2d1b65
eric9883 <eric9883@naver.com>
2022-07-15 16:59:00 +0900

    http request, member

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -5,10 +5,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    private static final String TAG = "HttpControllerTest: ";
+
+    /*
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+        Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+        System.out.println(TAG + "getter: "+m.getUsername());
+
+        return "get request";
+    }*/
+
     //http://localhost:8080/http/get
     @GetMapping("/http/get")
-    public String getTest(){
-        return "get request";
+    public String getTest(Member m){
+
+        return "get request: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
     //http://localhost:8080/http/post
