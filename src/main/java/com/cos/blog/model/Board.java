b5ce1b88bbaeafa11ade87f357a47eabc89b127f
eric9883 <eric9883@naver.com>
2022-07-18 11:24:53 +0900

    Table creation: Board

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,5 +1,36 @@
 package com.cos.blog.model;
 
-public class Board
-{
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content; // summer note library
+                            // <html> is mixed with design
+
+    @ColumnDefault("0")
+    private int count; // view count
+
+    @ManyToOne //Many = Board, User = One
+    @JoinColumn(name = "userId")
+    private User user;
+
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+
+
 }
