b2172afba4c011f2ec4c20072701939ba7e524a7
eric9883 <eric9883@naver.com>
2022-07-18 09:51:51 +0900

    lombok-test

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -7,18 +7,20 @@ public class HttpControllerTest {
 
     private static final String TAG = "HttpControllerTest: ";
 
-    /*
     @GetMapping("/http/lombok")
     public String lombokTest(){
-        Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
-        System.out.println(TAG + "getter: "+m.getUsername());
+        Member m = Member.builder().username("ssar").password("11234").email("ssar@nate.com").build();
+        System.out.println(TAG + "Getter" + m.getId());
+        m.setId(5000);
+        System.out.println(TAG + "setter" + m.getId());
 
-        return "get request";
-    }*/
+        return "lombok test complete";
+    }
 
     //http://localhost:8080/http/get
     @GetMapping("/http/get")
     public String getTest(Member m){
+
 
         return "get request: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
@@ -26,6 +28,7 @@ public class HttpControllerTest {
     //http://localhost:8080/http/post
     @PostMapping("/http/post") //text/plain, application/json
     public String postTest(@RequestBody Member m){ //MessageConverter(스프링부트)
+
         return "post request: " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
     }
 
