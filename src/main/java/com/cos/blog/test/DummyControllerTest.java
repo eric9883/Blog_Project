f0e3068f2d8e4f7e1202098cdccae6a74fe2a6bc
eric9883 <eric9883@naver.com>
2022-07-18 17:31:12 +0900

    Dummy Data Paging

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,14 +4,62 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @RestController
 public class DummyControllerTest {
 
     @Autowired  //  dependency injection
     private UserRepository userRepository;
+
+    @GetMapping("/dummy/users")
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC)Pageable pageable){
+        Page<User> pagingUser = userRepository.findAll(pageable);
+
+        List<User> users = pagingUser.getContent();
+
+        return users;
+    }
+
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id){
+
+        /*
+        //  Same as the below ElseThrow.
+        //  expression of RAMDA expression
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("No Exact User. ID: " + id);
+        });
+        */
+
+
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+            @Override
+            public IllegalArgumentException get() {
+                return new IllegalArgumentException("No Exact User. ID: " + id);
+            }
+        });
+
+
+
+        return user;
+    }
 
 
     //  http://localhost:8000/blog/dummy/join (request)
