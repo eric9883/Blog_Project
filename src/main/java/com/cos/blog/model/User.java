a13581080cda9bdf38d4f72e328346afeb5d7583
eric9883 <eric9883@naver.com>
2022-07-18 10:54:44 +0900

    Table creation: User

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,35 @@
+package com.cos.blog.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@Entity //  automatically create TABLE on MySQL
+public class User {
+
+    @Id//  primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; // sequence, auto_increment 넘버링
+
+    @Column(nullable = false, length = 30)
+    private String username; //ID
+
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; //     Enum type is better, ex) admin, user, manager
+
+
+    @CreationTimestamp //automatically input time stamp
+    private Timestamp createDate;
+
+}
---
+++ src/main/java/com/cos/blog/test/TempControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TempControllerTest {
+    public String tempHome() {
+        return "/home.html";
+
+    }
+
+
+}
