67bb9b7c7e2b60715cea627963a3871bbbe83833
eric9883 <eric9883@naver.com>
2022-07-19 13:47:23 +0900

    update user, delete user Completed

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,32 +4,70 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+
 @RestController
 public class DummyControllerTest {
+
 
     @Autowired  //  dependency injection
     private UserRepository userRepository;
 
+
+
+    @DeleteMapping("/dummy/user/{id}")
+    public String delete(@PathVariable int id) {
+
+        try{
+            userRepository.deleteById(id);
+
+        } catch(Exception e){
+            return "error";
+        }
+
+        return id + " Delete Completed";
+    }
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        System.out.println("id: " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("amendment failure!!");
+        });
+
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        userRepository.save(user);
+        //      -> dirty checking
+        return user;
+
+    }
+
+
     @GetMapping("/dummy/users")
-    public List<User> list(){
+    public List<User> list() {
         return userRepository.findAll();
     }
 
     @GetMapping("/dummy/user")
-    public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC)Pageable pageable){
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<User> pagingUser = userRepository.findAll(pageable);
 
         List<User> users = pagingUser.getContent();
@@ -38,12 +76,12 @@ public class DummyControllerTest {
     }
 
     @GetMapping("/dummy/user/{id}")
-    public User detail(@PathVariable int id){
+    public User detail(@PathVariable int id) {
 
         /*
         //  Same as the below ElseThrow.
         //  expression of RAMDA expression
-        User user = userRepository.findById(id).orElseThrow(()->{
+            User user = userRepository.findById(id).orElseThrow(()->{
             return new IllegalArgumentException("No Exact User. ID: " + id);
         });
         */
@@ -57,7 +95,6 @@ public class DummyControllerTest {
         });
 
 
-
         return user;
     }
 
@@ -65,7 +102,7 @@ public class DummyControllerTest {
     //  http://localhost:8000/blog/dummy/join (request)
     //  username, password, email data request to http Body
     @PostMapping("dummy/join")
-    public String join(User user){ //key = value
+    public String join(User user) { //key = value
 
         System.out.println("id: " + user.getId());
         System.out.println("username: " + user.getUsername());
@@ -81,4 +118,14 @@ public class DummyControllerTest {
         return "user sign up(join) complete.";
 
     }
+
+
+
+
+
+
+
 }
+
+
+
