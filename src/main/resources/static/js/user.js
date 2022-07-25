630c64323277ac8a4ba53ae1a6c565d59af4c06f
eric9883 <eric9883@naver.com>
2022-07-25 16:48:51 +0900

    join response

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -10,9 +10,25 @@ let index = {
             username: $("#username").val(),
             password: $("#password").val(),
             email: $("#email").val(),
+            contentType: "application/json; charset=utf-8", // notify what BODY data type is (MIME)
+            dataType: "json"    // response from server
 
         }
-        console.log(data);
+
+        $.ajax({
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data), //Javascript Language to JAVA => JSON string
+            contentType:"application/json; charset=utf-8",
+            dataType:"json"
+        }).done(function(resp){
+            alert("Join Completed!!");
+            location.href ="/blog";
+
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+
+        });
     }
 
 }
